77bb79014df4f4c070833823a228e2877e0cdd77
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2025-04-07 09:47:55 -0400

    Update debian package building for Noble

--- setup.py
+++ setup.py
@@ -1,3 +1,75 @@
+#!/usr/bin/env python
+import os
 from setuptools import setup
+import subprocess
 
-setup()
+def get_version():
+    """
+    Get the version from a git tag
+
+    Executes
+        git tag |grep -Eo '[0-9]+\\.[0-9]+\\.[0-9]+' |sort | tail -1
+    and returns the result
+    """
+    tag_proc = subprocess.Popen(
+        ["git", "tag"],
+        cwd=".",
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    )
+    grep_proc = subprocess.Popen(
+        [
+            "grep",
+            "-Eo",
+            "[0-9]+\\.[0-9]+\\.[0-9]+"
+        ],
+        stdin=tag_proc.stdout,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    )
+    sort_proc = subprocess.Popen(
+        ["sort"],
+        stdin=grep_proc.stdout,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    )
+    tail_proc = subprocess.Popen(
+        ["tail", "-1"],
+        stdin=sort_proc.stdout,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    )
+    (stdout, _) = tail_proc.communicate()
+    stdout = stdout.decode()
+    return stdout.strip()
+
+
+def enumerate_modules() -> list[str]:
+    """
+    Enumerate all additional modules that need to be included in the package.
+
+    :return: The list of modules that should be included in the package
+    """
+    modules = []
+    dotpy = ".py"
+    for root_dir, _, files in os.walk("canopen"):
+        for f in files:
+            if f.endswith(dotpy):
+                modules.append(
+                    f"{root_dir.replace('/', '.')}.{f.rstrip(dotpy)}"
+                )
+    return modules
+
+
+setup(
+    name="canopen",
+    version=get_version(),
+    description="CANopen stack implementation",
+    url="https://github.com/christiansandberg/canopen",
+    author="Christian Sandberg",
+    author_email="christiansandberg@me.com",
+    license="MIT",
+    py_modules=enumerate_modules(),
+    install_requires=[],
+)
